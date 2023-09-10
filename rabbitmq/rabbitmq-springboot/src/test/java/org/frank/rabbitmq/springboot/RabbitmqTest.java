543843d9d3a6ac91ab0798edd993a2f314e00aaf
fengandzhy <fengandzhy@gmail.com>
2023-09-10 13:50:11 +1200

    rabbitmq-springboot basic project

--- rabbitmq/rabbitmq-springboot/src/test/java/org/frank/rabbitmq/springboot/RabbitmqTest.java
+++ rabbitmq/rabbitmq-springboot/src/test/java/org/frank/rabbitmq/springboot/RabbitmqTest.java
@@ -37,4 +37,15 @@ public class RabbitmqTest {
         }));
         template.convertAndSend(env.getProperty("mq.config.exchange"), "order.new","新订单");
     }
+    
+    @Test
+    public void testReturnCallback() {
+        // 这个方法只有在消息失败的时候才会去调用, 从exchange 到 queue 失败的时候会调用
+        template.setReturnsCallback(returned -> {
+            System.out.println("returned="+returned.toString());
+        });
+        // 这里模拟一条失败消息, 找不到 routingKey 的 
+        template.convertAndSend(env.getProperty("mq.config.exchange"), "order1.new","从ReturnCallback那里来的新订单");
+    }    
+    
 }
