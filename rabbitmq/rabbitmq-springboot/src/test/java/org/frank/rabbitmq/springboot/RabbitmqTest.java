624ee0d82f5c883b74479ee546f7313d58c787e3
fengandzhy <fengandzhy@gmail.com>
2023-09-10 13:29:25 +1200

    rabbitmq-springboot basic project

--- rabbitmq/rabbitmq-springboot/src/test/java/org/frank/rabbitmq/springboot/RabbitmqTest.java
+++ rabbitmq/rabbitmq-springboot/src/test/java/org/frank/rabbitmq/springboot/RabbitmqTest.java
@@ -16,9 +16,25 @@ public class RabbitmqTest {
     private Environment env;
 
     @Test
-    void testSend() {
+    public void testSend() {
         for(int i=0;i<5;i++){
             template.convertAndSend(env.getProperty("mq.config.exchange"), "order.new","新订单");
         }
+    }
+    
+    @Test
+    public void testConfirmCallback() {
+        template.setConfirmCallback(((correlationData, ack, cause) -> {
+            System.out.println("correlationData="+correlationData);
+            System.out.println("ack="+ack);
+            System.out.println("cause="+cause);
+            
+            if(ack){
+                System.out.println("消息发送成功, 数据库状态已经更新为发送成功");
+            }else{
+                System.out.println("消息发送失败, 数据库状态已经更新为发送失败");
+            }
+        }));
+        template.convertAndSend(env.getProperty("mq.config.exchange"), "order.new","新订单");
     }
 }
