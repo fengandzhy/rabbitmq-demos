441e6d6eaa7516ad8c5ce5a7d84eab7357795680
fengandzhy <fengandzhy@gmail.com>
2023-09-11 15:36:46 +1200

    rabbitmq-advanced basic project

---
+++ rabbitmq/rabbitmq-advanced/src/main/java/org/frank/rabbitmq/advanced/limit/consumer/MyCommonConsumer.java
@@ -0,0 +1,37 @@
+package org.frank.rabbitmq.advanced.limit.consumer;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+
+import java.io.IOException;
+
+public class MyCommonConsumer extends DefaultConsumer {
+
+    private Channel channel;
+
+    /**
+     * Constructs a new instance and records its association to the passed-in channel.
+     *
+     * @param channel the channel to which this consumer is attached
+     */
+    public MyCommonConsumer(Channel channel) {
+        super(channel);
+        this.channel = channel;
+    }
+
+    @Override
+    public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+
+        System.out.println("收到消息："+new String(body));
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        channel.basicAck(envelope.getDeliveryTag(),false); //这里的false表示不批量签收
+    }
+}
